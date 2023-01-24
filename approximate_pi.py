c1def6769402e7cb3522933212f34e4c3cf74b04
Sriya Reddy Adunur <sadunur@unix1.csc.calpoly.edu>
2023-01-24 12:13:37 -0800

    Added prompt to approximate pi.

--- approximate_pi.py
+++ approximate_pi.py
@@ -12,4 +12,41 @@ pi = perimeter / 2
 
 # NOTE: Only round the approximation to the desired precision when printing. Do
 #       not use the rounded value for subsequent iterations.
-print("pi is approximately " + str(round(pi, 2)) + ".")
+
+iterations = int(input("How many iterations should be performed? "))
+decimals = int(input("How many decimal places should be reported? "))
+
+if iterations < 4:
+    print("Error: Cannot perform < 4 iterations.")
+
+elif decimals > 7: 
+    print("Warning: Cannot report > 7 decimal places.")
+    print("pi is approximately " + str(round(pi, 7)) + ".")
+
+    for i in range(iterations - 1):
+        side_length = (((side_length ** 2)/4 + ((1 - ((1 - ((side_length ** 2)/4)) ** .5)) ** 2)) ** .5)
+        num_sides = num_sides * 2
+        new_perimeter = num_sides * side_length
+        new_pi = new_perimeter/2
+        print("pi is approximately " + str(round(new_pi, 7)) + ".")
+
+elif decimals < 0:
+    print("Warning: Cannot report < 0 decimal places.")
+    print("pi is approximately " + str(round(pi, 0)) + ".")   
+
+    for i in range(iterations - 1):
+        side_length = (((side_length ** 2)/4 + ((1 - ((1 - ((side_length ** 2)/4)) ** .5)) ** 2)) ** .5)
+        num_sides = num_sides * 2
+        new_perimeter = num_sides * side_length
+        new_pi = new_perimeter/2
+        print("pi is approximately " + str(round(new_pi, 0)) + ".")
+
+else:
+    print("pi is approximately " + str(round(pi, decimals)) + ".")
+    
+    for i in range(iterations - 1):
+        side_length = (((side_length ** 2)/4 + ((1 - ((1 - ((side_length ** 2)/4)) ** .5)) ** 2)) ** .5)
+        num_sides = num_sides * 2
+        new_perimeter = num_sides * side_length
+        new_pi = new_perimeter/2
+        print("pi is approximately " + str(round(new_pi, decimals)) + ".")
